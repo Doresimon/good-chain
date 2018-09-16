81cf58fcff5e4660d7e90bd5797d572d063976d9
Doresimon <doresimon@163.com>
2018-09-16 22:07:10 +0800

    [test]

--- db/db.go
+++ db/db.go
@@ -2,16 +2,7 @@ package main
 
 import (
 	"fmt"
-	"errors"
 	"github.com/syndtr/goleveldb/leveldb"
-)
-
-var (
-    ErrNotFound         = errors.ErrNotFound
-    ErrReadOnly         = errors.New("leveldb: read-only mode")
-    ErrSnapshotReleased = errors.New("leveldb: snapshot released")
-    ErrIterReleased     = errors.New("leveldb: iterator released")
-    ErrClosed           = errors.New("leveldb: closed")
 )
 
 func main(){
