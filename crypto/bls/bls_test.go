40500f5194a202d161968fabe575d262cf092c12
Doresimon <doresimon@163.com>
2019-08-04 02:11:52 +0800

    add crypto/bls

---
+++ crypto/bls/bls_test.go
@@ -0,0 +1,82 @@
+package bls
+
+import (
+	"math/big"
+	"strconv"
+	"testing"
+
+	"golang.org/x/crypto/bn256"
+)
+
+func Test_BLS_Short_Signature_Scheme(t *testing.T) {
+	// key generation
+	sk, pk := KeyGenerate()
+	// signing
+	msg := []byte("hello world")
+	sig := Sign(sk, msg)
+	// verifying
+	ok := Verify(pk, msg, sig)
+	if !ok {
+		t.Error("verification failed.")
+	}
+}
+func Test_BLS_Aggregate_Signature_Scheme(t *testing.T) {
+	N := 64
+	sks := make([]*big.Int, N, N)
+	pks := make([]*bn256.G2, N, N)
+	msgs := make([][]byte, N, N)
+	sigs := make([]*bn256.G1, N, N)
+
+	for i := 0; i < N; i++ {
+		sks[i], pks[i] = KeyGenerate()
+		msgs[i] = []byte("hello world" + strconv.Itoa(i))
+		sigs[i] = Sign(sks[i], msgs[i])
+	}
+
+	asig, err := Aggregate(sigs)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ok, err := AVerify(asig, msgs, pks)
+	if err != nil {
+		t.Error(err)
+	}
+	if !ok {
+		t.Error("aggregate signature verification failed.")
+	}
+}
+
+func Benchmark_KeyGenerate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		KeyGenerate()
+	}
+}
+func Benchmark_Sign(b *testing.B) {
+	sk, _ := KeyGenerate()
+	msg := []byte("hello world")
+	for i := 0; i < b.N; i++ {
+		Sign(sk, msg)
+	}
+}
+func Benchmark_Verify(b *testing.B) {
+	sk, pk := KeyGenerate()
+	msg := []byte("hello world")
+	sig := Sign(sk, msg)
+	for i := 0; i < b.N; i++ {
+		Verify(pk, msg, sig)
+	}
+}
+
+func Benchmark_Short_Scheme(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sk, pk := KeyGenerate()
+		KeyGenerate()
+		msg := []byte("hello world")
+		sig := Sign(sk, msg)
+		ok := Verify(pk, msg, sig)
+		if !ok {
+			b.Error("verification failed.")
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/libp2p/go-libp2p-protocol v0.1.0
 	github.com/multiformats/go-multiaddr v0.0.4
 	github.com/syndtr/goleveldb v1.0.0
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
 	gopkg.in/urfave/cli.v1 v1.20.0
